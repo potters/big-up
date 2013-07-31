c8e84a808a1666e51df030918b921014684529da
Myles Byrne <myles@myles.id.au>
2013-07-31 14:13:19 -0500

    Fixed bug with root route, factored all routes into own module

---
+++ router.js
@@ -0,0 +1,79 @@
+/*
+
+JTBD: match request handling functions to appropriate path strings
+
+* Each hanlder function gets a http req and res object so knows that interface
+
+*/
+
+
+var mapleTree = require('mapleTree')
+var router = new mapleTree.RouteTree()
+
+module.exports = router
+
+router.rootHandler = function(req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'})
+  require('fs').createReadStream('README.md','utf8').pipe(res)
+}
+
+var css = require('./css')
+
+router.define('/:name.css', function(req, res) {
+  var name = this.params.name
+    , next = this.cbs.length ? this.next : notFound
+
+  css(name, function(exists, readStream) {
+    if (!exists) { next(req, res) ; return }
+    res.writeHead(200, {'Content-Type': 'text/css'})
+    readStream.pipe(res)
+  })
+})
+
+var tvScreen = require('./tv-screen')
+
+router.define('/tv', function(req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'})
+  tvScreen().pipe(res)
+})
+
+function notFound(req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'})
+  res.end('NF\n')
+}
+
+router.notFound = notFound
+
+// var postStore = require('./post-store')
+
+// router.define('/posts/latest', function(req, res) {
+//   res.writeHead(200, {'Content-Type': 'text/html'})
+//   postStore.latest(20).pipe(jsonStringify).pipe(res)
+// })
+
+// var jsonStringify = require('node-stream-stringify')
+
+// router.define('/posts/latest', function(req, res) {
+//   res.writeHead(200, {'Content-Type': 'text/html'})
+//   postStore.latest(20).pipe(jsonStringify).pipe(res)
+// })
+
+// router.define('/posts', function(req, res) {
+//   var next = this.cbs.length ? this.next : notFound
+//   if (!req.method === "POST") { next(req, res) ; return }
+
+//   // var newPost = parse and filter params ...
+//   // validate newPost ...
+//   postStore.create(newPost, function(err) {
+//     if (err) { throw err } // not good enough, there may be other open connections
+//     res.writeHead(200, {'Content-Type': 'text/html'})
+//     res.end('OK\n')
+//   })
+// })
+
+// var leaderboard = require('leaderboard')
+
+// router.define('/leaderboard', function(req, res) {
+//   res.writeHead(200, {'Content-Type': 'text/html'})
+//   leaderboard(10).pipe(jsonStringify).pipe(res)
+// })
--- server.js
+++ server.js
@@ -5,89 +5,32 @@ JTDB: serve the app over http
 */
 
 var http = require('http')
+  , router = require('./router')
 
 var server = http.createServer(function(req, res) {
 
   // Home page straight to the brain
-  if (req.url === '/') {
-    res.writeHead(200, {'Content-Type': 'text/plain'})
-    require('fs').readFileSync('README.md')
+  if (req.method === 'GET' && req.url === '/') {
+    router.rootHandler(req, res)
     return
   }
 
   // Otherwise, one of the other pages or no page
   var routeMatch = router.match(req.url)
   if (routeMatch.perfect) routeMatch.fn(req, res)
-  else notFound(req, res)
+  else router.notFound(req, res)
 
 })
 
-server.listen(process.env.PORT || 4000)
+var port = process.env.PORT || 4000
+
+server.listen(port, function() {
+  console.log("listening on port", port)
+})
 
 // ---
-
-var mapleTree = require('mapleTree')
-var router = new mapleTree.RouteTree()
-
-var css = require('./css')
-
-router.define('/:name.css', function(req, res) {
-  var name = this.params.name
-    , next = this.cbs.length ? this.next : notFound
-
-  css(name, function(exists, readStream) {
-    if (!exists) { next(req, res) ; return }
-    res.writeHead(200, {'Content-Type': 'text/css'})
-    readStream.pipe(res)
-  })
-})
-
-var tvScreen = require('./tv-screen')
-
-router.define('/tv', function(req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'})
-  tvScreen().pipe(res)
-})
-
-// var postStore = require('./post-store')
-
-// router.define('/posts/latest', function(req, res) {
-//   res.writeHead(200, {'Content-Type': 'text/html'})
-//   postStore.latest(20).pipe(jsonStringify).pipe(res)
-// })
-
-// var jsonStringify = require('node-stream-stringify')
-
-// router.define('/posts/latest', function(req, res) {
-//   res.writeHead(200, {'Content-Type': 'text/html'})
-//   postStore.latest(20).pipe(jsonStringify).pipe(res)
-// })
-
-// router.define('/posts', function(req, res) {
-//   var next = this.cbs.length ? this.next : notFound
-//   if (!req.method === "POST") { next(req, res) ; return }
-
-//   // var newPost = parse and filter params ...
-//   // validate newPost ...
-//   postStore.create(newPost, function(err) {
-//     if (err) { throw err } // not good enough, there may be other open connections
-//     res.writeHead(200, {'Content-Type': 'text/html'})
-//     res.end('OK\n')
-//   })
-// })
-
-// var leaderboard = require('leaderboard')
-
-// router.define('/leaderboard', function(req, res) {
-//   res.writeHead(200, {'Content-Type': 'text/html'})
-//   leaderboard(10).pipe(jsonStringify).pipe(res)
-// })
 
 // webscocket server, multiplexed, then routed to:
 // /posts/since/:id
 // /leaderboard
 
-function notFound(req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'})
-  res.end('NF\n')
-}
