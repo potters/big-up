c0c3b6b7a895a26ec46a63f1445bb0fc972b91bb
Myles Byrne <myles@myles.id.au>
2013-07-31 10:23:40 -0500

    A module for streaming out the styles

---
+++ css.js
@@ -0,0 +1,33 @@
+/*
+
+JTBD: stream out the app's stylesheets
+
+Takes a path, returns a readable stream. The path must point to a css file in
+this folder.
+
+todo: caching
+
+*/
+
+var fs = require('fs')
+
+module.exports = streamForName
+
+function streamForName(name, cb) {
+  var path = './'+name+'.css'
+  fs.exists(path, function(exists){
+    if (!exists) { cb(exists); return }
+    cb(exists, fs.createReadStream(path))
+  })
+}
+
+if (!module.parent) {
+  // Run with: node css.js base
+
+  var name = process.argv[2]
+
+  streamForName(name, function(exists, str) {
+    if (!exists) throw new Error(name + " 'aint no css known about 'ere")
+    str.pipe(process.stdout)
+  })
+}
